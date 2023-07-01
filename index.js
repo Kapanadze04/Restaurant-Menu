580c24e32908d380bf57bd476b403a456d5fd862
kapanadze04 <kapanadze.gio96@gmail.com>
2023-07-01 18:30:46 +0400

    Total cost work

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const orderPizza = document.querySelector(".order-pizza");
 const pizzaPrice = document.querySelector("#pizza-price");
 const removePizza = document.querySelector(".remove-pizza");
 
-
 const addBurger = document.querySelector("#add-burger");
 const orderBurger = document.querySelector(".order-burger");
 const burgerPrice = document.querySelector("#burger-price");
@@ -18,7 +17,7 @@ const orderTitle = document.querySelector(".order-title");
 const orderFood = document.querySelector(".order-food");
 const total = document.querySelector(".total");
 const completeBtn = document.querySelector(".complete");
-// const totalPrice = document.querySelector(".total-price");
+const totalPrice = document.querySelector(".total-price");
 
 
 let price1 = 0;
@@ -34,11 +33,24 @@ addBeer.addEventListener("click", function() {
 
     price3 += 12;
     beerPrice.textContent = "$" + price3;
+    totalPrice.textContent = "$" + (price1 + price2 + price3);
 })
 
 removeBeer.addEventListener("click", function() {
     price3 -= 12;
     beerPrice.textContent = "$" + price3;
+    totalPrice.textContent = "$" + (price1 + price2 + price3);
+
+    if(price3 < 12) {
+        orderBeer.style.display = "none";
+    }
+
+    if(price1 < 14 && price2 < 12 && price3 < 12) {
+        orderTitle.style.display = "none";
+        orderFood.style.display = "none";
+        total.style.display = "none";
+        completeBtn.style.display = "none";
+    }
 })
 
 addBurger.addEventListener("click", function() {
@@ -50,11 +62,24 @@ addBurger.addEventListener("click", function() {
 
     price2 += 12;
     burgerPrice.textContent = "$" + price2;
+    totalPrice.textContent = "$" + (price1 + price2 + price3);
 })
 
 removeBurger.addEventListener("click", function() {
     price2 -= 12;
     burgerPrice.textContent = "$" + price2;
+    totalPrice.textContent = "$" + (price1 + price2 + price3);
+
+    if(price2 < 12) {
+        orderBurger.style.display = "none";
+    }
+    
+    if(price1 < 14 && price2 < 12 && price3 < 12) {
+        orderTitle.style.display = "none";
+        orderFood.style.display = "none";
+        total.style.display = "none";
+        completeBtn.style.display = "none";
+    }
 })
 
 addPizza.addEventListener("click", function() {
@@ -66,16 +91,22 @@ addPizza.addEventListener("click", function() {
 
     price1 += 14;
     pizzaPrice.textContent = "$" + price1;
+    totalPrice.textContent = "$" + (price1 + price2 + price3);
 })
 
 removePizza.addEventListener("click", function() {
     price1 -= 14;
     pizzaPrice.textContent = "$" + price1;
+    totalPrice.textContent = "$" + (price1 + price2 + price3);
+
+    if(price1 < 14) {
+        orderPizza.style.display = "none";
+    }
+
+    if(price1 < 14 && price2 < 12 && price3 < 12) {
+        orderTitle.style.display = "none";
+        orderFood.style.display = "none";
+        total.style.display = "none";
+        completeBtn.style.display = "none";
+    }
 })
-
-
-
-
-
-
-
